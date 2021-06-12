fa574e8dcc609a775b15f6c56a12d6b700cee6cc
Sveinar Søpler <cybermax@dexter.no>
2021-06-12 14:14:52 +0200

    Add NvAPI_GPU_GetBusId

--- nvtt.c
+++ nvtt.c
@@ -9,18 +9,21 @@ typedef int (*NvAPI_Initialize_t)();
 typedef int (*NvAPI_Unload_t)();
 typedef int (*NvAPI_EnumPhysicalGPUs_t)(int **handles, int *count);
 typedef int (*NvAPI_GPU_GetFullName_t)(int *handle, char *sysname);
+typedef int (*NvAPI_GPU_GetBusId_t)(int *handle, NvU32 *pBusId);
 
 NvAPI_QueryInterface_t NvQueryInterface = 0;
 NvAPI_Initialize_t NvInit = 0;
 NvAPI_Unload_t NvUnload = 0;
 NvAPI_EnumPhysicalGPUs_t NvEnumGPUs = 0;
 NvAPI_GPU_GetFullName_t NvGetName = 0;
+NvAPI_GPU_GetBusId_t BusId = 0;
 
 int main(int argc, char **argv)
 {
     int nGPU=0;
     int *hdlGPU[64]={0};
     char sysname[64]={0};
+    NvU32 pBusId;
 
     NvAPI_Status ret = NVAPI_OK;
 
@@ -29,12 +32,17 @@ int main(int argc, char **argv)
     NvUnload        = NvQueryInterface(0xD22BDD7E);
     NvEnumGPUs      = NvQueryInterface(0xE5AC921F);
     NvGetName       = NvQueryInterface(0xCEEE8E9F);
+    BusId           = NvQueryInterface(0x1BE0B8E5);
 
     NvInit();
     NvEnumGPUs(hdlGPU, &nGPU);
     ret = NvGetName(hdlGPU[0], sysname);
     if(ret == NVAPI_OK){
       printf("Name: %s\n", sysname);
+    }
+    ret = BusId(hdlGPU[0], &pBusId);
+    if(ret == NVAPI_OK){
+      printf("BusId: %ld\n", pBusId);
     }
     NvUnload();
 
