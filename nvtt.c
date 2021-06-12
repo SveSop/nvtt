ecfaf2dd6241de085bd1b94d56abd93accf66cd0
Sveinar Søpler <cybermax@dexter.no>
2021-06-12 14:44:45 +0200

    NvAPI_GPU_GetPCIIdentifiers

--- nvtt.c
+++ nvtt.c
@@ -13,6 +13,7 @@ typedef int (*NvAPI_GPU_GetBusId_t)(int *handle, NvU32 *pBusId);
 typedef int (*NvAPI_GPU_GetGPUType_t)(int *handle, int *pGpuType);
 typedef int (*NvAPI_GetDisplayDriverVersion_t)(int *handle, NV_DISPLAY_DRIVER_VERSION *pVersion);
 typedef int (*NvAPI_GPU_GetPhysicalFrameBufferSize_t)(int *handle, int *memsize);
+typedef int (*NvAPI_GPU_GetPCIIdentifiers_t)(int *handle, NvU32 *pDeviceId, NvU32 *pSubSystemId, NvU32 *pRevisionId, NvU32 *pExtDeviceId);
 
 NvAPI_QueryInterface_t NvQueryInterface = 0;
 NvAPI_Initialize_t NvInit = 0;
@@ -23,13 +24,14 @@ NvAPI_GPU_GetBusId_t BusId = 0;
 NvAPI_GPU_GetGPUType_t NvGPUType = 0;
 NvAPI_GetDisplayDriverVersion_t NvDriver = 0;
 NvAPI_GPU_GetPhysicalFrameBufferSize_t NvGetMemSize = 0;
+NvAPI_GPU_GetPCIIdentifiers_t NvPCIID = 0;
 
 int main(int argc, char **argv)
 {
     int nGPU=0, pGpuType=0, memsize=0;
     int *hdlGPU[64]={0}, *hdlDisp[32]={0};
     char sysname[64]={0};
-    NvU32 pBusId;
+    NvU32 pBusId, pDeviceId, pSubSystemId, pRevisionId, pExtDeviceId;
     NV_DISPLAY_DRIVER_VERSION pVersion;
     pVersion.version = NV_DISPLAY_DRIVER_VERSION_VER;
 
@@ -44,6 +46,7 @@ int main(int argc, char **argv)
     NvGPUType       = NvQueryInterface(0xc33baeb1);
     NvDriver        = NvQueryInterface(0xF951A4D1);
     NvGetMemSize    = NvQueryInterface(0x46FBEB03);
+    NvPCIID         = NvQueryInterface(0x2ddfb66e);
 
     NvInit();
     NvEnumGPUs(hdlGPU, &nGPU);
@@ -75,6 +78,17 @@ int main(int argc, char **argv)
     ret = NvGetMemSize(hdlGPU[0], &memsize);
     if(ret == NVAPI_OK){
       printf("VRAM: %dMB\n", memsize/1024);
+    }
+    ret = NvPCIID(hdlGPU[0], &pDeviceId, &pSubSystemId, &pRevisionId, &pExtDeviceId);
+    if(ret == NVAPI_OK){
+      u_int ven=(pDeviceId >> 16);
+      short dev=pDeviceId;
+      printf("DeviceID: %04X-%04X\n", dev, ven);
+      u_int subven=(pSubSystemId >> 16);
+      short subdev=pSubSystemId;
+      printf("Subdevice ID: %04X-%04X\n", subdev, subven);
+      printf("Revision ID: %lX\n", pRevisionId);
+      printf("Ext Device ID: %lX\n", pExtDeviceId);
     }
     NvUnload();
 
